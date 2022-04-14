11e3649d2c75684f3df2f111f05ab51f0857cf4a
Karl Jensen <karljensen@gmail.com>
2022-04-14 14:02:44 -0700

    Clean up

--- part-1/tictactoe/scripts.ts
+++ part-1/tictactoe/scripts.ts
@@ -3,5 +3,7 @@ export default {
     test: "deno test",
     coverage:
       "rm -rf coverage && deno test --coverage=coverage && deno coverage coverage",
+    report:
+      "deno coverage coverage --lcov > coverage/coverage.lcov && genhtml -o coverage/html coverage/coverage.lcov",
   },
 };
